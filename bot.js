37cee8a336b8861b239136ee9369ed01f73b0ef8
Ryu <42417276+notsoaccount@users.noreply.github.com>
2018-10-26 15:25:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ invisible.on("message", async msg => {
                                     var m = await msgs.map(m => `${++x}. ${m.author.tag} : ${m.content.split(" ").join(" ")}`).join("\n")
                                     var l = `# The last msg in ${invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).name} is the first one here..\n`
                                     await hastebin(l + m, "md").then(async url => {
-                                        return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send(`\`\`\`py\nBulked ${msgs.size}/${count} msgs..\n# ${url}\n\`\`\``)
+                                        return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send(`\`\`\`py\nBulked ${msgs.size} msgs for "You"..\n# ${url}\n\`\`\``)
                                     })
                                 } else {
                                     if (msgs.size == 0) return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send(`\`\`\`py\nI cant find any msg for "${mention.user.tag}" to "${command}"\n# You should to know that i can't ${command} the very old msgs..\n\`\`\``).then(async m => m.delete(4333))
@@ -86,7 +86,7 @@ invisible.on("message", async msg => {
                                     var m = await msgs.map(m => `${++x}. ${m.author.tag} : ${m.content.split(" ").join(" ")}`).join("\n")
                                     var l = `# The last msg in ${invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).name} is the first one here..\n`
                                     await hastebin(l + m, "md").then(async url => {
-                                        return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send(`\`\`\`py\nBulked ${msgs.size}/${count} msgs..\n# ${url}\n\`\`\``)
+                                        return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send(`\`\`\`py\nBulked ${msgs.size} msgs for ${mention.tag}..\n# ${url}\n\`\`\``)
                                     })                                
                                 }                  
                             })
