845c1add2ed9ada6bee7b9dcb32eec9763943a6b
Ryu <42417276+notsoaccount@users.noreply.github.com>
2018-10-26 14:54:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ invisible.on("message", async msg => {
                             else {
                                 var x = 0
                                 var m = await msgs.map(m => `${++x}. ${m.author.tag} : ${m.content.split(" ").join(" ")}`).join("\n")
-                                var l = "# The last bulked msg is the first one here.. "
+                                var l = "# The last bulked msg is the first one here..\n"
                                 await hastebin(l + m, "md").then(async url => {
                                     return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send(`\`\`\`py\nBulked ${msgs.size}/${count} msgs..\n# ${url}\n\`\`\``)
                                 })
