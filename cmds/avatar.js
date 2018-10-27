9ae0d4c258bf0e910fe6bcb6e81a06804dd93655
Ryu <42417276+notsoaccount@users.noreply.github.com>
2018-10-27 17:36:55 +0200

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,8 +1,5 @@
 module.exports.run = async (msg, args, command, invisible, O) => {
-  invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send({
-  File : msg.author.avatarURL,
-  Name : msg.author.username
-})
+  invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).sendFile(msg.author.avatarURL)
 }
 exports.help = {
   "name" : "avatar"
