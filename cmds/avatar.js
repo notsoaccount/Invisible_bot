bfca53d38272624ddac20cec8003b582b2dd0edb
Ryu <42417276+notsoaccount@users.noreply.github.com>
2018-10-27 19:48:51 +0200

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,9 +1,1 @@
-module.exports.run = async (invisible, msg, command ,args, cooldown, secs, O) => {
-  return invisible.guilds.get(msg.guild.id).channels.get(msg.channel.id).send({
-    File : msg.author.avatarURL,
-    Name : msg.author.username
-})
-}
-exports.help = {
-  "name" : "avatar"
-}
+
