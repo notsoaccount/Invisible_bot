50445a23373eda360f1c9c7c4668db06c8404557
Ryu <42417276+notsoaccount@users.noreply.github.com>
2018-10-27 17:45:36 +0200

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,5 +1,5 @@
 module.exports.run = async (invisible, msg, command ,args, cooldown, secs, O) => {
-  msg.channel.send({File : msg.author.avatarURL})
+  msg.channel.send({File : `${msg.author.avatarURL}`})
 }
 exports.help = {
   "name" : "avatar"
